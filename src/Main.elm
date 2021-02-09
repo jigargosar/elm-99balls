0ee94199dba4dc91b8b377920e5d9cc0b8b8a9a3
Jigar Gosar <jigar.gosar@gmail.com>
2021-02-09 14:53:14 +0530

    init

--- src/Main.elm
+++ src/Main.elm
@@ -22,7 +22,7 @@ import Util exposing (..)
   - [x] implement TestMovingSphereSphere (RTC) and compute new ball velocity against single static ball.
       - [x] same with static balls list.
 
-  - fix: multiple collision i.e.
+  - fix: multiple collision i.e
 
   - fix: ball edge tunnelling problem, since we are not doing continuous collision
 
