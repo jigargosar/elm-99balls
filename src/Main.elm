b451c58817202ae60deb6dfedbb68842a24c7ad3
Jigar Gosar <jigar.gosar@gmail.com>
2021-02-10 21:30:23 +0530

    init

--- src/Main.elm
+++ src/Main.elm
@@ -371,7 +371,7 @@ updateBallHelp targets ball =
     let
         velocity =
             ballVelocity ball
-                |> vecMapY (add 0.1)
+                |> vecMapY (add 0.01)
     in
     case detectBallCollision targets velocity ball of
         Nothing ->
