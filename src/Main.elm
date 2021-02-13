6ff41096f882a3afef9c651e902dd65e1c869888
Jigar Gosar <jigar.gosar@gmail.com>
2021-02-13 20:02:02 +0530

    init

--- src/Main.elm
+++ src/Main.elm
@@ -832,12 +832,12 @@ view model =
         , Px.height sh
         , S.fill "none"
         , S.stroke "none"
-        , E.preventDefaultOn "pointerdown" (JD.succeed ( PointerDown True, True ))
-        , E.preventDefaultOn "pointerup" (JD.succeed ( PointerDown False, True ))
+        , E.preventDefaultOn "pointerdown" (JD.succeed ( PointerDown True, False ))
+        , E.preventDefaultOn "pointerup" (JD.succeed ( PointerDown False, False ))
         , E.preventDefaultOn "pointermove"
             (JD.map2 vec (JD.field "offsetX" JD.float) (JD.field "offsetY" JD.float)
                 |> JD.map PointerMoved
-                |> JD.map (pairTo True)
+                |> JD.map (pairTo False)
             )
         , style "touch-action" "none"
         , style "use-select" "none"
