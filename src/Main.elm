f6273b98754f848e549a597996fa0c4b51a9cfea
Jigar Gosar <jigar.gosar@gmail.com>
2021-02-16 02:43:51 -0800

    init

--- src/Main.elm
+++ src/Main.elm
@@ -57,6 +57,8 @@ import Util exposing (..)
 
   - [x] +2: Annoying. Bug: circle seg collision is causing penetration, maybe try seg-seg intersection.
 
+  - bug: initial ball position is outside edges.
+
   - test input handling on phone/touch device.
       - scale view to mobile screen
       - improve accuracy,
