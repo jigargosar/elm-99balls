39b0f04cfaf608c0f4ccc3a387c1c6be70d11865
Jigar Gosar <jigar.gosar@gmail.com>
2021-02-27 15:36:19 +0530

    trying rollup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// import {Elm} from "./main.elm.js"
+//
+// Elm.Main.init()
+
 // import _ from "./main.elm.js"
 //
 // console.log(_);
 
 // noinspection ES6CheckImport
-import {Elm} from "./Main.elm"
+import Elm from "./Main.elm"
+
 
 Elm.Main.init()
