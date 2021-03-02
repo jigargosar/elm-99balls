0b23e002e8d3efb11508c51fdfb188354be3ed9f
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-02 13:57:21 +0530

    play sample sound

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import "./styles.css"
+// noinspection ES6CheckImport
 import {Elm} from "./Main.elm"
 // noinspection ES6UnusedImports
 import {Howl, Howler} from 'howler';
 
 
-const app = Elm.Main.init({node: document.getElementById('root')});
+const app = Elm["Main"].init({
+    node: document.getElementById('root')
+});
 
-const sound = new Howl({
-    src: ['snd1.ogg']
-})
 
-app.ports.playSound.subscribe(function () {
-    sound.play()
-})
+app.ports["playSound"]["subscribe"]((function () {
+    const sound = new Howl({src: ['snd1.ogg']})
+    return function () {
+        sound.play()
+    }
+})())
+
