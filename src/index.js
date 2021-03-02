489e656959bcfe014b76da0ab4ba53b4e19c3bb7
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-02 15:53:00 +0530

    play hit sound

--- src/index.js
+++ src/index.js
@@ -12,13 +12,25 @@ const app = Elm["Main"].init({
 app.ports["playSound"]["subscribe"]((function () {
     const shoot = new Howl({src: ['snd_swoosh.ogg']})
     const hit = new Howl({src: ['snd_hit.ogg']})
+    const kill1 = new Howl({src: ['snd_kill_1.ogg']})
+    const kill2 = new Howl({src: ['snd_kill_2.ogg']})
+    const kill3 = new Howl({src: ['snd_kill_3.ogg']})
     return function (name) {
-        switch (name){
+        switch (name) {
             case 'shoot' :
                 shoot.play()
                 break
             case 'hit' :
                 hit.play()
+                break
+            case 'kill_1' :
+                kill1.play()
+                break
+            case 'kill_2' :
+                kill2.play()
+                break
+            case 'kill_3' :
+                kill3.play()
                 break
         }
     }
