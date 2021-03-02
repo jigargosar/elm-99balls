0b0cbdd9d70be32ff6cd5697fb4409d69163e6ee
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-02 17:14:22 +0530

    play hit sound

--- src/index.js
+++ src/index.js
@@ -9,7 +9,35 @@ const app = Elm["Main"].init({
     node: document.getElementById('root')
 });
 
+
+function mapValues(fn, obj) {
+    return Object
+        .keys(obj)
+        .reduce(
+            (acc, k) => Object.assign(acc, {[k]: fn(obj[k])}),
+            {},
+        )
+}
+
+
 app.ports["playSound"]["subscribe"]((function () {
+    const soundPathMapping =
+        {
+            shoot: 'snd_swoosh.ogg',
+            hit: 'snd_hit.ogg',
+            kill_1: 'snd_kill_1.ogg',
+            kill_2: 'snd_kill_2.ogg',
+            kill_3: 'snd_kill_3.ogg',
+            kill_4: 'snd_kill_4.ogg',
+            kill_5: 'snd_kill_5.ogg',
+            kill_6: 'snd_kill_6.ogg',
+            kill_7: 'snd_kill_7.ogg',
+            kill_8: 'snd_kill_8.ogg',
+        }
+
+    const sounds = mapValues((v) => new Howl({src: [v]}), soundPathMapping)
+
+
     const shoot = new Howl({src: ['snd_swoosh.ogg']})
     const hit = new Howl({src: ['snd_hit.ogg']})
     const kill1 = new Howl({src: ['snd_kill_1.ogg']})
