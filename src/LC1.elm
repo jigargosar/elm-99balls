362abfb062fd5d08fca15d9d5f6a1aa606fe14fd
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-07 15:56:32 +0530

    plot random functions for improving understanding

--- src/LC1.elm
+++ src/LC1.elm
@@ -2,6 +2,8 @@ module LC1 exposing (..)
 
 import Html
 import LineChart
+import LineChart.Colors
+import LineChart.Dots
 import List.Extra
 import Random exposing (Generator)
 import Random.Float
@@ -17,24 +19,44 @@ type alias Point =
     { x : Int, y : Int }
 
 
+type alias BellPts =
+    { n2 : List Point
+    , n3 : List Point
+    , n4 : List Point
+    }
+
+
 chart : Html.Html msg
 chart =
-    LineChart.view1
+    let
+        ( bellPts, _ ) =
+            Random.step rndBellPts (Random.initialSeed 0)
+    in
+    LineChart.view
         (.x >> toFloat)
         (.y >> toFloat)
-        --[ Point 1 2, Point 5 5, Point 10 1 ]
-        (Random.step rndPoints (Random.initialSeed 0)
-            |> Tuple.first
-            |> List.sortBy .x
-        )
+        [ LineChart.dash LineChart.Colors.pink LineChart.Dots.none "N2" [ 4, 2 ] bellPts.n2
+        , LineChart.line LineChart.Colors.blue LineChart.Dots.none "N3" bellPts.n3
+        , LineChart.line LineChart.Colors.gold LineChart.Dots.none "N4" bellPts.n4
+        ]
 
 
-rndPoints =
-    Random.list 50000 (rndBell 3 -10 10 |> rnd1 round)
+rndBellPts : Generator BellPts
+rndBellPts =
+    Random.map3 BellPts
+        (rndPointsN 2)
+        (rndPointsN 3)
+        (rndPointsN 4)
+
+
+rndPointsN : Int -> Generator (List Point)
+rndPointsN n =
+    Random.list 50000 (rndBellMO n 0 20 |> rnd1 round)
         |> Random.map
             (List.Extra.gatherEquals
                 >> List.map
                     (\( x, xs ) ->
                         Point x (List.length xs)
                     )
+                >> List.sortBy .x
             )
