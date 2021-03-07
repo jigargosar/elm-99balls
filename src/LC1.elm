ead4a9cc970b09b753da55ff7b837dadb1920839
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-07 16:09:09 +0530

    plot random functions for improving understanding

--- src/LC1.elm
+++ src/LC1.elm
@@ -23,6 +23,7 @@ type alias BellPts =
     { b2 : List Point
     , b3 : List Point
     , b4 : List Point
+    , b5 : List Point
     , normal : List Point
     }
 
@@ -36,19 +37,23 @@ bellChart =
     LineChart.view
         (.x >> toFloat)
         (.y >> toFloat)
-        [ LineChart.dash LineChart.Colors.pink LineChart.Dots.none "N2" [ 4, 2 ] bellPts.b2
-        , LineChart.line LineChart.Colors.blue LineChart.Dots.none "N3" bellPts.b3
-        , LineChart.line LineChart.Colors.gold LineChart.Dots.none "N4" bellPts.b4
+        [ LineChart.line LineChart.Colors.blue LineChart.Dots.none "Bell3" bellPts.b3
+        , LineChart.line LineChart.Colors.pink LineChart.Dots.none "Bell2" bellPts.b2
+        , LineChart.line LineChart.Colors.gold LineChart.Dots.none "Bell4" bellPts.b4
+        , LineChart.line LineChart.Colors.black LineChart.Dots.none "Bell5" bellPts.b5
+
+        --, LineChart.line LineChart.Colors.cyan LineChart.Dots.none "Normal" bellPts.normal
         ]
 
 
 rndBellPts : Generator BellPts
 rndBellPts =
-    Random.map4 BellPts
+    Random.map5 BellPts
         (rndPointsN 2)
         (rndPointsN 3)
         (rndPointsN 4)
-        (Random.list 50000 (Random.Float.normal 0 20 |> rnd1 round)
+        (rndPointsN 5)
+        (Random.list 50000 (Random.Float.normal 0 (20 / 4) |> rnd1 round)
             |> Random.map
                 (List.Extra.gatherEquals
                     >> List.map
