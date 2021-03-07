36098674767cced309732325111a6791a88b53e6
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-07 14:19:18 +0530

    plot random functions for improving understanding

---
+++ src/LC1.elm
@@ -0,0 +1,21 @@
+module LC1 exposing (..)
+
+import Html
+import LineChart
+
+
+main : Html.Html msg
+main =
+    chart
+
+
+type alias Point =
+    { x : Float, y : Float }
+
+
+chart : Html.Html msg
+chart =
+    LineChart.view1
+        .x
+        .y
+        [ Point 1 2, Point 5 5, Point 10 1 ]
