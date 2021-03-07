3d8847217d5df698154553b99ec423894bf2db68
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-07 14:36:17 +0530

    plot random functions for improving understanding

--- src/LC1.elm
+++ src/LC1.elm
@@ -2,6 +2,9 @@ module LC1 exposing (..)
 
 import Html
 import LineChart
+import List.Extra
+import Random
+import Random.Float
 
 
 main : Html.Html msg
@@ -18,4 +21,18 @@ chart =
     LineChart.view1
         .x
         .y
-        [ Point 1 2, Point 5 5, Point 10 1 ]
+        --[ Point 1 2, Point 5 5, Point 10 1 ]
+        (Random.step rndPoints (Random.initialSeed 0) |> Tuple.first)
+
+
+rndPoints =
+    Random.list 1000 Random.Float.standardNormal
+        |> Random.map
+            (identity
+                >> List.Extra.gatherEqualsBy (\x -> x * 10 |> round)
+                >> List.sortBy Tuple.first
+                >> List.map
+                    (\( x, xs ) ->
+                        Point x (List.length xs |> toFloat)
+                    )
+            )
