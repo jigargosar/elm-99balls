618f183c5ecaee74b5a079a5791da063cb798803
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-15 16:09:45 +0530

    state simplification

--- src/Main.elm
+++ src/Main.elm
@@ -1191,8 +1191,8 @@ subscriptions _ =
     Sub.batch
         [ Browser.Events.onAnimationFrameDelta (always OnTick)
             -- for monitors where refresh rate is >60 i.e. 144
-            -- since we are not using delta for updates.
-            -- also high refresh rate can lead to too many updates
+            -- since we are not using `delta` for updates.
+            -- also high refresh rate can lead to many updates
             -- perhaps, causing high load, in addition to its
             -- inherent implementation complexity.
             |> always (Time.every (1000 / 60) (always OnTick))
