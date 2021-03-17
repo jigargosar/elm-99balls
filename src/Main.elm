a168d3b102f54d4957a3e8414db7ba2b57e96369
Jigar Gosar <jigar.gosar@gmail.com>
2021-03-17 16:18:11 +0530

    mute

--- src/Main.elm
+++ src/Main.elm
@@ -765,7 +765,7 @@ update message (Model env page) =
                 mute =
                     not env.mute
             in
-            ( Model { env | mute = mute } page, Cmd.batch [ setMute mute, playSound "btn" ] )
+            ( Model { env | mute = mute } page, Cmd.batch [ setMute mute ] )
 
         RestartGameClicked ->
             let
